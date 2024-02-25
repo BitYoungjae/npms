9527110c4a0e96335d9a20121f241e49c17c3f16
BitYoungjae <bityoungjae@gmail.com>
2024-02-25 18:03:37 +0900

    chore: minor improvements
    KO
    - 스크립트 내용을 설치 스크립트에 직접 포함시키지 않고 저장소에서 다운로드 받아 쓰도록 개선
    - 가독성 향상을 위해 Early return으로 변경
    - life cycle ops를 필터링하는 과정에서 grep을 안 쓰고 jq를 쓰도록 변경.
    
    EN
    - Improved to use the script from the repository instead of including it directly in the installation script.
    - Improved readability by changing to Early return
    - Changed to use jq instead of grep to filter life cycle ops.

--- install.sh
+++ install.sh
@@ -1,43 +1,21 @@
 #!/bin/bash
 
-# Content to be installed
-NPMS_CONTENT='
-# npms-script-start::1.0.0
-run_npm_script() {
-  # Finds the path to the nearest package.json in the current directory
-  local package_json_path=$(npm prefix --silent)/package.json
+NPMS_SCRIPT_URL=https://raw.githubusercontent.com/bityoungjae/npms/main/npms.sh
 
-  if [[ ! -f "$package_json_path" ]]; then
-    echo "Cannot find package.json file."
-    return
-  fi
+# Fetch the content to be installed from the remote script
+NPMS_CONTENT=$(curl -fs $NPMS_SCRIPT_URL)
 
-  # Excludes life cycle operations from the list.
-  local life_cycle_ops="(preinstall|install|postinstall|preuninstall|uninstall|postuninstall|preversion|version|postversion|pretest|posttest|prepublish|prepare|prepublishOnly|postpublish|prepack|postpack|prestop|stop|poststop|prestart|start|poststart|prerestart|restart|postrestart)"
-  local name=$(jq -r ".scripts | keys | .[]" "$package_json_path" | grep -vE $life_cycle_ops | fzf)
+# Check if fzf and jq are installed
+if ! command -v fzf &> /dev/null || ! command -v jq &> /dev/null; then
+  echo "Cannot find jq and fzf. Please install these required packages first."
+  exit 1
+fi
 
-  if [[ -n "$name" ]]; then
-    echo "Running '$name'"
-
-    local package_lock_path=$(npm prefix --silent)/package-lock.json
-    local yarn_lock_path=$(npm prefix --silent)/yarn.lock
-    local pnpm_lock_path=$(npm prefix --silent)/pnpm-lock.yaml
-
-    if [[ -f "$package_lock_path" ]]; then
-      npm run $name
-    elif [[ -f "$yarn_lock_path" ]]; then
-      yarn $name
-    elif [[ -f "$pnpm_lock_path" ]]; then
-      pnpm $name
-    else
-      echo "Cannot determine the package manager to execute the script."
-    fi
-  else
-    echo "No script selected."
-  fi
-}
-alias npms="run_npm_script"
-# npms-script-end'
+# Check if the curl command succeeded
+if [ $? -ne 0 ]; then
+  echo "Failed to fetch the npms script."
+  exit 1
+fi
 
 # Check current shell and select appropriate profile file
 if [[ "$SHELL" == */bash ]]; then
@@ -46,26 +24,24 @@ elif [[ "$SHELL" == */zsh ]]; then
   PROFILE_FILE="$HOME/.zshrc"
 else
   echo "Unsupported shell. Please manually install using the script linked below."
-  echo "https://github.com/BitYoungjae/npms/blob/main/npms.sh"
+  echo $NPMS_SCRIPT_URL
   exit 1
 fi
 
-# Check if fzf and jq are installed
-if ! command -v fzf &> /dev/null || ! command -v jq &> /dev/null; then
-  echo "Cannot find jq and fzf. Please install these required packages first."
+# Checking if the npms script is already added
+if grep -q "# npms-script-start" "$PROFILE_FILE"; then
+  echo "npms alias is already added to $PROFILE_FILE."
   exit 1
-else
-  # Checking if the npms script is already added
-  if ! grep -q "# npms-script-start" "$PROFILE_FILE"; then
-    echo "Adding npms alias to $PROFILE_FILE.."
-    # If the file does not end with a newline, add one at the beginning.
-    if [[ ! "$(tail -c 1 "$PROFILE_FILE" | tr -d '[:space:]')" == "" ]]; then
-        echo "" >> "$PROFILE_FILE"
-    fi
-    echo "$NPMS_CONTENT" >> "$PROFILE_FILE"
-    echo "Added successfully."
-    echo "Please run 'source $PROFILE_FILE' and then type 'npms' in the terminal."
-  else
-    echo "npms alias is already added to $PROFILE_FILE."
-  fi
 fi
+
+echo "Adding npms alias to $PROFILE_FILE.."
+
+# If the file does not end with a newline, add one.
+if [[ "$(tail -c 1 "$PROFILE_FILE")" != "" ]]; then
+  echo >> "$PROFILE_FILE"
+fi
+
+echo >> "$PROFILE_FILE"
+echo "$NPMS_CONTENT" >> "$PROFILE_FILE"
+echo "Added successfully."
+echo "Please run 'source $PROFILE_FILE' and then type 'npms' in the terminal."
--- npms.sh
+++ npms.sh
@@ -9,9 +9,9 @@ run_npm_script() {
   fi
 
   # Excludes life cycle operations from the list.
-  local life_cycle_ops="(preinstall|install|postinstall|preuninstall|uninstall|postuninstall|preversion|version|postversion|pretest|posttest|prepublish|prepare|prepublishOnly|postpublish|prepack|postpack|prestop|stop|poststop|prestart|start|poststart|prerestart|restart|postrestart)"
-  local name=$(jq -r ".scripts | keys | .[]" "$package_json_path" | grep -vE $life_cycle_ops | fzf)
-
+  local life_cycle_ops_json='["preinstall", "install", "postinstall", "preuninstall", "uninstall", "postuninstall", "preversion", "version", "postversion", "pretest", "posttest", "prepublish", "prepare", "prepublishOnly", "postpublish", "prepack", "postpack", "prestop", "stop", "poststop", "prestart", "start", "poststart", "prerestart", "restart", "postrestart"]'
+  local name=$(jq -r --argjson life_cycle_ops "$life_cycle_ops_json" '.scripts | keys | map(select(. as $x | $life_cycle_ops | index($x) | not))[]' "$package_json_path" | fzf)
+  
   if [[ -n "$name" ]]; then
     echo "Running '$name'"
 
